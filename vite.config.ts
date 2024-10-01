fd37f009e34212afe9a13620b208a109e0c91b32
Steve ZARETTI <kossolax@gmail.com>
2024-10-01 08:23:25 +0200

    added shadcn

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import path from "path"
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { checker } from 'vite-plugin-checker'
@@ -15,4 +16,9 @@ export default defineConfig({
       }
     }),
   ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
