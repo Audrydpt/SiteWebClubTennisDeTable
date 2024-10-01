6d79591db321cda730ec1b69b155a05ce45c7b96
Steve ZARETTI <kossolax@gmail.com>
2024-10-01 09:04:16 +0200

    changed uri

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,28 @@
 import path from "path"
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import { checker } from 'vite-plugin-checker'
 
-
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    checker({
-      typescript: true,
-      eslint: {
-        lintCommand: 'eslint "./src/**/*.{js,jsx,ts,tsx}"',
-        useFlatConfig: true,
-      }
-    }),
-  ],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    base: env.BASE_URL || '/',
+    plugins: [
+      react(),
+      checker({
+        typescript: true,
+        eslint: {
+          lintCommand: 'eslint "./src/**/*.{js,jsx,ts,tsx}"',
+          useFlatConfig: true,
+        }
+      }),
+    ],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
     },
-  },
-})
+  };
+});
