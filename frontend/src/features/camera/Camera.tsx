c1c9cbbd60e22fed8e871f4440e866863a482f1c
Steve ZARETTI <kossolax@gmail.com>
2024-12-05 11:08:41 +0100

    license check

---
+++ frontend/src/features/camera/Camera.tsx
@@ -0,0 +1,37 @@
+export default function Camera() {
+  const streamId = 1;
+  let url = new URL(window.location.href);
+
+  try {
+    url = new URL(process.env.MAIN_API_URL || '');
+  } catch {
+    url = new URL(window.location.href);
+  }
+
+  const serverUrl = url.origin;
+
+  return (
+    <div className="flex justify-center">
+      <table className="w-[900px]">
+        <tbody>
+          <tr>
+            <td className="w-1/2 align-top text-center">
+              <iframe
+                title="results"
+                className="w-full h-[425px]"
+                src={`${serverUrl}/cgi-bin/CameraView.cgi?stream=${streamId}`}
+              />
+            </td>
+            <td>
+              <iframe
+                title="cameraForm"
+                className="w-full h-[610px]"
+                src={`${serverUrl}/cgi-bin/CameraForm.cgi?stream=${streamId}`}
+              />
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+}
