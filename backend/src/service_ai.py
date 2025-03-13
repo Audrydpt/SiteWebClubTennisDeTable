feb0337d35ff6d864242134fab2a545315163335
Samy Bakkouche <sbakkouche@chapsvision.com>
2025-03-13 16:30:38 +0100

    Added timeout, support for multihead models and classification parsing

--- backend/src/service_ai.py
+++ backend/src/service_ai.py
@@ -52,7 +52,24 @@ class ServiceAI:
         del copy["msg"]
         return copy
     
-    async def process(self, image):
+    async def parse_classif_response(self, classif_response,sort=False):
+        parsed_classif_response = []
+        for classif in classif_response: # for all bbox detections
+            parsed_classif = {}
+            for head in classif: # for all classification heads in the model
+                
+                head_name, head_values = list(head.items())[0]
+                if sort :
+                    #sort the classes by values
+                    head_values = {k: v for k, v in sorted(head_values.items(), key=lambda item: item[1], reverse=True)}
+
+
+                parsed_classif[head_name] = head_values
+
+            parsed_classif_response.append(parsed_classif)
+        return parsed_classif_response
+    
+    async def process(self, image,selected_classes=["car","truck","bus","motorcycle","bicycle"]):
         print("Processing Forensic")
 
         version = await self.get_version()
@@ -64,9 +81,12 @@ class ServiceAI:
         models_list = list(models.keys())
         
         obj_model = [model for model in models_list if "/object" in model.lower()][0]
-        color_model = [model for model in models_list if "/color" in model.lower()][0]
+
+        """ color_model = [model for model in models_list if "/color" in model.lower()][0]
         type_model = [model for model in models_list if "/type" in model.lower()][0]
-        classif_models = [color_model, type_model]
+        classif_models = [color_model, type_model] """
+        vehicule_model = [model for model in models_list if "/vehicule" in model.lower()][0]
+        classif_models = [vehicule_model]
 
         obj_modelWidth = models[obj_model]["networkWidth"]
         obj_modelHeight = models[obj_model]["networkHeight"]
@@ -88,12 +108,12 @@ class ServiceAI:
             image = np.concatenate([image, padding], axis=1)
 
 
-        image_raw = cv2.cvtColor(image, cv2.COLOR_BGR2YUV_I420).astype("uint8")#.flatten().astype("uint8")
+        image_raw = cv2.cvtColor(image, cv2.COLOR_BGR2YUV_I420).astype("uint8")
 
         obj_response = await self.__detect_object(width=obj_modelWidth, height=obj_modelHeight, raw=image_raw, object_detection=True,model=obj_model)
-        filtered_obj_response = [obj for obj in obj_response if "car" in obj["bbox"]["probabilities"].keys()]
-
-        print(f"{len(filtered_obj_response)} detection(s): ", filtered_obj_response)
+        
+        
+        filtered_obj_response = [obj for obj in obj_response if any(cls in obj["bbox"]["probabilities"].keys() for cls in selected_classes)]
 
         all_classif_response = []
         for model in classif_models:
@@ -104,18 +124,24 @@ class ServiceAI:
             i=0
             for res in filtered_obj_response:
                 
-                image_class = self.get_thumbnail(image,res,0.5)
+                image_class = self.get_thumbnail(image,res,0.)
                 image_class = cv2.resize(image_class, (class_modelWidth, class_modelHeight))
                 image_raw_class = cv2.cvtColor(image_class, cv2.COLOR_BGR2YUV_I420).astype("uint8")
                 
                 #save on disk
-                #cv2.imwrite(f"test_save_class_{i}.jpg", cv2.cvtColor(image_raw_class, cv2.COLOR_YUV2BGR_I420))
+                cv2.imwrite(f"test_save_class_{i}.jpg", cv2.cvtColor(image_raw_class, cv2.COLOR_YUV2BGR_I420))
                 i+=1
-                classif_response.append(await self.__detect_object(width=class_modelWidth, height=class_modelHeight, raw=image_raw_class, classification=True,model=model))
+                try:
+                    classif_response.append(await self.__detect_object(width=class_modelWidth, height=class_modelHeight, raw=image_raw_class, classification=True,model=model))
+                except Exception as e:
+                    logger.error(f"Error during classification detection: {e}")
+                    break
             
-            print(f"{len(classif_response)} Classification detection(s) for model {model}: ", classif_response)
+            parsed_classif_response = await self.parse_classif_response(classif_response,sort=True)
 
-            all_classif_response.append(classif_response)
+            all_classif_response.append(parsed_classif_response)
+        
+        return filtered_obj_response, all_classif_response
 
         
         
@@ -150,7 +176,6 @@ class ServiceAI:
             service_session = aiohttp.ClientSession()
             connection = await service_session.ws_connect(f"ws://{self.analytic}{model}/requestsQueue")
             self.ws[model] = (connection, service_session)
-        #return self.ws[model]
 
     def get_pixel_bbox(self, frame, detection):
         def getCoord(x, y, w, h):
@@ -189,8 +214,8 @@ class ServiceAI:
 
     async def __detect_object(self, object_detection=False, classification=False, width=0, height=0, raw=None, jpeg=None,model=None):
         ctx = {
-            "confidenceThreshold": 0.5,
-            "overlapThreshold": 0.0001,
+            "confidenceThreshold": 0.05,
+            "overlapThreshold": 0.1,
             "bbox": True if object_detection else False,
             "classifier": True if classification else False
         }
@@ -228,31 +253,75 @@ class ServiceAI:
         else:
           logger.error("No image provided")
           raise Exception("No image provided")
-                    
-        async for msg in self.ws[model][0]:
+        
+        try:
+            msg = await asyncio.wait_for(self.ws[model][0].receive(), timeout=1)
             if msg.type == aiohttp.WSMsgType.TEXT:
                 data = json.loads(msg.data)
                 #logger.info(f"Received: {data}")
                 if data["msg"] == "response":
 
                     # classifier has higher priority than obj detector
-                    if "classifiers" in data :
+                    if "classifiers" in data:
                         return data["classifiers"]
-                    
+
                     return data["detections"]
                 else:
                     logger.debug(data)
-        
+        except asyncio.TimeoutError:
+            logger.error("Timeout while waiting for response")
+            raise Exception("Timeout while waiting for response")
+                           
 
 async def main():
-    async with ServiceAI("192.168.20.220:53211") as forensic:
+    async with ServiceAI("127.0.0.1:53211") as forensic:
         print("Starting")
         await forensic.get_version()
-        print("Done")
 
+        #params
         test = cv2.imread("test.jpg")
-        detections = await forensic.process(test)
-        print(detections)
+        selected_classes = ["car","truck","bus","motorcycle","bicycle"]
+        short_print = True
+        nbr_classif_values = 3
+
+
+
+        detections,classifs = await forensic.process(test,selected_classes)
+
+
+        print(f"{len(detections)} objects detected and classified:")
+        for idx,(detection,classif) in enumerate(zip(detections,classifs[0])):
+            
+            if short_print:
+                prob = detection['bbox']['probabilities']
+                prob_class = list(prob.keys())[0]
+                prob_values = list(prob.values())[0]
+                x_min = detection['bbox']['min']['x']
+                y_min = detection['bbox']['min']['y']
+                x_max = detection['bbox']['max']['x']
+                y_max = detection['bbox']['max']['y']
+
+                x = (x_max + x_min) / 2
+                y = (y_max + y_min) / 2
+                w = x_max - x_min
+                h = y_max - y_min
+
+                print(f"{idx}) {prob_class} detected with {100 * prob_values:.2f}% confidence at position x:{x:.2f} - y:{y:.2f} - w:{w:.2f} - h:{h:.2f} ")
+                
+                for classification in classif:
+                    
+                    print(f"\t{classification} classification:")
+                    for i, (key, value) in enumerate(classif[classification].items()):
+                        if i >= nbr_classif_values:
+                            break
+                        print(f"\t\t| {key} - {100 * value:.2f}%")
+                
+                print("\n\n")
+            else:
+                print(detection)
+                print(classif)
+                print("\n\n")
+
         
         print("closing")
     print("completed")
