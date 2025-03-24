4e6c583c94611471308b59c5c81a2f87fa96b339
Steve ZARETTI <kossolax@gmail.com>
2025-03-24 10:56:58 +0100

    i18n of settings

--- frontend/src/features/settings/Settings.tsx
+++ frontend/src/features/settings/Settings.tsx
@@ -1,20 +1,25 @@
+import { useTranslation } from 'react-i18next';
+
 import Header from '@/components/header';
+
 import Retention from './Retention';
 import Users from './Users';
 
 function Settings() {
+  const { t } = useTranslation();
+
   return (
     <div>
-      <Header title="System Settings" />
+      <Header title={t('settings:headers.systemSettings')} />
       <section className="mb-6">
-        <Header title="Users management" level="h2" />
+        <Header title={t('settings:headers.usersManagement')} level="h2" />
         <div className="grid grid-cols-1 gap-4">
           <Users />
         </div>
       </section>
 
       <section className="mb-6">
-        <Header title="Retention Settings" level="h2" />
+        <Header title={t('settings:headers.retentionSettings')} level="h2" />
         <div className="grid grid-cols-1 gap-4">
           <Retention />
         </div>
