70a95711a226157c6bac6b49fbd13691ae663df9
Steve ZARETTI <kossolax@gmail.com>
2025-05-30 17:40:07 +0200

    updated component

--- frontend/src/hooks/use-mobile.tsx
+++ frontend/src/hooks/use-mobile.tsx
@@ -2,7 +2,8 @@ import * as React from 'react';
 
 const MOBILE_BREAKPOINT = 768;
 
-export default function useIsMobile() {
+// eslint-disable-next-line import/prefer-default-export
+export function useIsMobile() {
   const [isMobile, setIsMobile] = React.useState<boolean | undefined>(
     undefined
   );
