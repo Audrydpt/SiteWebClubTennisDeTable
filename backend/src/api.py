1d5a352979c543c1f63044e54a53ce0cf7935788
Steve ZARETTI <kossolax@gmail.com>
2025-06-02 16:23:33 +0200

    added missing diff

--- backend/src/api.py
+++ backend/src/api.py
@@ -51,6 +51,7 @@ logger.setLevel(logging.INFO)
 file_handler = logging.FileHandler(f"/tmp/{__name__}.log")
 file_handler.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %(message)s'))
 logger.addHandler(file_handler)
+logger.addHandler(logging.StreamHandler())
 
 class ModelName(str, Enum):
     minute = "1 minute"
